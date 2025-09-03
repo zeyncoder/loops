513f05f3cb303e49675b0759bba95224eb78e851
Zeynal <zeynal@Zeynals-MacBook-Air.local>
2025-09-03 14:06:06 +0400

    Loops

---
+++ src/Main.java
@@ -0,0 +1,71 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        for(int num = 1; num <= 15; num++) {
+            System.out.println(num+"kvadrati" + num*num);
+        }
+  }
+        /// ///// 50 den 1 e qeder 2 ye bolunsun
+//        double n = 50;
+//        while (n > 1) {
+//            System.out.println(n);
+//            n = n / 2;
+//        }
+        ////////////////////// 100 e qeder dongu donsun
+//        Scanner scanner = new Scanner(System.in);
+//        int number;
+//        do{
+//            number = scanner.nextInt();
+//        }while(number <=100);
+//        scanner.close();
+//        System.out.println("okey");
+        ////////////////////20 e qeder yazdir
+//    int counter=1;
+//    while (counter <=20) {
+//        System.out.println(counter);
+//        counter++;
+//    }
+    /////////////////// gizli reqemi tap
+
+//       Scanner scanner = new Scanner(System.in);
+//       int secretnumber =27;
+//       int number;
+//       do{
+//           System.out.println("Secret number?");
+//           number = scanner.nextInt();
+//
+//       }while(secretnumber != number);
+//        System.out.println("Secret number: " + secretnumber);
+        /// ////////////// 15 e qeder kvadratlari
+//for(int num = 1; num <= 15; num++) {
+//    System.out.println(num+"kvadrat"+ num*num);
+//
+//
+// }
+        /// ///////// sheherleri cap etdir
+//        String[] cities = {"Baku","Qakh","Zagatala"};
+//        for (String city : cities) {
+//            System.out.println(city);
+//        }
+        ///////////// 100 e qeder say 42 de dayan
+//    for(int i=1;i<=100;i++) {
+//        if(i==42) {
+//            break;
+//        }
+//        System.out.println(i);
+//    }
+        /// ///////////// 30 a qeder 5 e bolunenleri kec
+//    for(int z = 0; z <= 30; z++) {
+//        if(z % 5 == 0) {
+//            continue;
+//        }
+//        System.out.println(z);
+//
+//    }
+    }
+
+
---
+++ src/yg/Rna.java
@@ -0,0 +1,22 @@
+package yg;
+
+import java.util.Scanner;
+
+public class Rna {
+    public static void main(String[] args) {
+        for(int number = 1; number <= 20; number++) {
+            if(number==15){
+                System.out.println(" men 15 i kecdim");
+                continue;
+
+            }else if(number==17) {
+                System.out.println(" men 17 de dayandim");
+                break;
+            }
+
+            System.out.println(number);
+        }
+
+        }
+        }
+
